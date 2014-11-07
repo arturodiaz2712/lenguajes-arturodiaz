f9c017ca9e2a844d0b69b08cf3686f8e4582e9ed
Telcel <Telcel@10.60.1.40>
2014-11-06 20:45:32 -0600

    clase_del_6_noviembre

---
+++ JavaApplication1/src/excepciones.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Telcel
+ */
+public class excepciones {
+    public void hola()
+   {
+    System.out.println("hola mundo");
+   }
+}
